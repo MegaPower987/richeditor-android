9223a7f2d456dc2b1499c78f29560eca9a9790bb
MegaPower987 <cr.esmaeeili@yahoo.com>
2020-07-22 00:19:28 +0430

    Update rich_editor.js

--- richeditor/src/main/assets/rich_editor.js
+++ richeditor/src/main/assets/rich_editor.js
@@ -182,12 +182,12 @@ RE.insertImage = function(url, alt) {
 }
 
 RE.insertVideo = function(url, alt) {
-    var html = '<video src="' + url + '" controls/> &nbsp; <br>';
+    var html = '<video src="' + url + '" controls/>&nbsp;';
     RE.insertHTML(html);
 }
 
 RE.insertAudio = function(url, alt) {
-    var html = '<audio src="' + url + '" controls/> &nbsp; <br>';
+    var html = '<audio src="' + url + '" controls/>&nbsp;';
     RE.insertHTML(html);
 }
 
