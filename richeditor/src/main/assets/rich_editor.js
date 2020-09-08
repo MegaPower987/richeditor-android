eb6e9c99975139432a6b273c5c84d7332bceea2c
MegaPower987 <cr.esmaeeili@yahoo.com>
2020-09-08 19:52:49 +0430

    Update rich_editor.js

--- richeditor/src/main/assets/rich_editor.js
+++ richeditor/src/main/assets/rich_editor.js
@@ -177,7 +177,7 @@ RE.setBlockquote = function() {
 }
 
 RE.insertImage = function(url, alt) {
-    var html = '<img src="' + url + '" alt="' + alt + '" width="500" />';
+    var html = '<img src="' + url + '" alt="' + alt + '" width="220" />';
     RE.insertHTML(html);
 }
 
